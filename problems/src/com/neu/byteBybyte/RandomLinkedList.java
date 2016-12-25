46c97ce9ecd59943664f66e8845699f23bb2d0d5
Raghavendra venkatesh <raghucse@ccs.neu.edu>
2016-12-25 18:50:59 -0500

    MergeSort with some doubt

---
+++ problems/src/com/neu/byteBybyte/RandomLinkedList.java
@@ -0,0 +1,56 @@
+package com.neu.byteBybyte;
+
+/**
+ * Created by raghu on 12/25/2016.
+ */
+class RandomNode{
+    int data;
+    RandomNode next;
+    RandomNode Random;
+
+    RandomNode(int data)
+    {
+        this.data = data;
+    }
+}
+
+public class RandomLinkedList {
+    public static void main(String[] args) {
+
+    }
+
+    public RandomNode clone(RandomNode n)
+    {
+        RandomNode cur = n ;
+
+        while (cur != null)
+        {
+            RandomNode temp = new RandomNode(cur.data);
+            temp.next = cur.next;
+            cur.next = temp;
+            cur = cur.next.next;
+        }
+
+        cur = n;
+
+        while (cur != null)
+        {
+            cur.next.Random = cur.Random.next;
+            cur = cur.next.next;
+        }
+
+        RandomNode copy = cur.next;
+        cur = n;
+        while (cur != null) {
+            RandomNode temp = cur.next;
+            temp.next = temp.next.next;
+
+            cur.next = cur.next.next;
+            cur = cur.next;
+
+        }
+
+        return copy;
+
+    }
+}
