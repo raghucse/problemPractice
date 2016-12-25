0fe68a4f745d96111f6eb29f69f7410733a81411
Raghavendra venkatesh <raghucse@ccs.neu.edu>
2016-12-24 22:41:07 -0500

    MergeSort with some doubt

---
+++ problems/src/com/neu/byteBybyte/MergeArray.java
@@ -0,0 +1,78 @@
+package com.neu.byteBybyte;
+
+/**
+ * Created by raghu on 12/23/2016.
+ */
+public class MergeArray {
+    public static void main(String[] args) throws Exception{
+        int a[] = new int[]{1,3,6,0,0,0};
+        int b[] = new int[]{2,4,5};
+
+           new MergeArray().mergeArray(a, b);
+
+    }
+
+    public void mergeArray(int a[], int b[]) throws Exception
+    {
+
+
+        int shortAray[] ;
+        int longArray[];
+
+        int i ;
+        int j ;
+        int k ;
+
+        if(a.length < b.length)
+        {
+            shortAray = a;
+            longArray = b;
+        }
+        else
+        {
+            shortAray = b;
+            longArray = a;
+        }
+
+            // throw exception id not enough space
+        k = longArray.length-1;
+        j = shortAray.length-1;
+
+        i = 0;
+        while(a[i+1] != 0)
+        {
+            i++;
+        }
+
+        while (j >= 0 && i >=0)
+        {
+            if(shortAray[j] > longArray[i])
+            {
+                longArray[k] = shortAray[j];
+                j--;
+                k--;
+            }
+            else
+            {
+                longArray[k] = longArray[i];
+                i--;
+                k--;
+            }
+        }
+
+        while (j>=0)
+        {
+            longArray[k] = shortAray[j];
+            j--;
+            k--;
+        }
+
+
+
+        for (int l  = 0; l < longArray.length; l++)
+        {
+            System.out.println(longArray[l]);
+        }
+
+    }
+}
