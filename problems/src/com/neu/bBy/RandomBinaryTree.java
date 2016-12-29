a83a4c19feba1cf7817844fc2f7e9b12587b6e44
Raghavendra venkatesh <raghucse@ccs.neu.edu>
2016-12-28 20:19:49 -0500

    Random Binary Tree

---
+++ problems/src/com/neu/bBy/RandomBinaryTree.java
@@ -0,0 +1,43 @@
+package com.neu.bBy;
+
+import java.util.Random;
+
+/**
+ * Created by raghu on 12/28/2016.
+ */
+class RBNode{
+    RBNode left;
+    RBNode right;
+    int val;
+    int children;
+}
+
+public class RandomBinaryTree {
+    private RBNode root;
+    private Random rand;
+
+    public static void main(String[] args) {
+
+    }
+
+    public int  getRandomNode()
+    {
+        if(root == null) throw new NullPointerException();
+        int count = rand.nextInt(root.children + 1);
+        return getRandomNode(root, count);
+    }
+
+    public int getRandomNode(RBNode curr, int count)
+    {
+        if(count == children(curr.left)) return curr.val;
+        if(count < children(curr.left)) return  getRandomNode(curr.left,count);
+        return getRandomNode(curr.right, count-children(curr.left)-1);
+    }
+
+    public int children(RBNode n)
+    {
+        if(n == null) return 0;
+        return n.children +1 ;
+    }
+
+}
